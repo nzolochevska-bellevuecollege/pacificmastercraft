d82240f7d1264076693c3c7ef83fb4c1c9872cd4
nzolochevska-bellevuecollege <natalia.zolochevska@bellevuecollege.edu>
2025-12-22 23:42:37 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: '/', // IMPORTANT for custom domain
+  base: '/pacificmastercraft/', // IMPORTANT for custom domain
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
